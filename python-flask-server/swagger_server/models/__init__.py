41decbafbe16bdb2b6fe2a903ff24e68f4367b62
Loic Laureote <laureote-loic@hotmail.com>
2020-04-17 11:48:17 +0200

    add swagger

---
+++ python-flask-server/swagger_server/models/__init__.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from swagger_server.models.api_response import ApiResponse
+from swagger_server.models.chat import Chat
+from swagger_server.models.entity import Entity
+from swagger_server.models.user import User
