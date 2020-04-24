4df8beb95bbc8814b7a75a2e02e34ecfd2e23e1d
JulienTech <34554597+JulienTech@users.noreply.github.com>
2020-04-24 20:10:52 +0200

    Animation Error

--- error.php
+++ error.php
@@ -24,6 +24,68 @@ body {
 .page-wrap {
 	min-height: 100vh;
 }
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+body {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: "Quicksand", sans-serif;
+  font-size: 42px;
+  background: #f7f7f7;
+}
+
+span {
+  margin: 0 15px;
+  line-height: 0.7;
+  text-shadow: 0 0 2px rgba(0, 0, 0, 0.45);
+  animation: span 3s ease-in infinite alternate;
+}
+
+.main {
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+}
+
+.letter {
+  display: inline-flex;
+  height: 30px;
+  width: 30px;
+  /* border: 2.5px solid #FF1EAD; */
+  border: 5px solid #363636;
+  border-radius: 40px;
+  box-shadow: 0 0 2px rgba(0, 0, 0, 0.75), inset 0 0 2px rgba(0, 0, 0, 0.45);
+  animation: letter 3s ease-in-out infinite alternate;
+}
+
+@keyframes span {
+  0%,
+  30% {
+    margin: 0 10px;
+  }
+  70%,
+  100% {
+    margin: 0 -2px;
+  }
+}
+@keyframes letter {
+  0%,
+  30% {
+    width: 30px;
+  }
+  70%,
+  100% {
+    width: 30vw;
+  }
+}
+
 	</style>
 </head>
 <body>
@@ -33,7 +95,16 @@ body {
 	        <div class="row justify-content-center">
 	            <div class="col-md-12 text-center">
 	            	<img src="/img/small_logo.png" alt="Logo">
-	                <span class="display-1 d-block"><?php echo strtoupper(search('error')).' '.$_GET['v'];?></span>
+	                <span class="display-1 d-block main">
+	                	<span class="main" style="font-size: 40px;">
+		                	<span>E</span>
+		                	<span>R</span>
+		                	<span>R</span>
+		                	<span class="letter"></span>
+		                	<span>R</span>
+		                </span>
+	                	<span><?php echo $_GET['v'];?></span>
+	                </span>
 	                <div class="mb-4 lead"><?php echo search('sorry').', '.search('la page que(.+)chargée').'.'; ?></div>
 	                <a href="/" class="btn btn-link"><?php echo search('Back to Home');?></a>
 	            </div>
